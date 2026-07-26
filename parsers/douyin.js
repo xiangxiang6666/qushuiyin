8845df2d58bd036c6da0db6b24d43c113f3b7ad6
xiangxiang6666 <872388587@qq.com>
2026-07-26 11:26:23 +0800

    Update douyin.js

--- parsers/douyin.js
+++ parsers/douyin.js
@@ -1,21 +1,98 @@
+const axios = require("axios");
+
+
 module.exports = {
 
-    name:"douyin",
+    name:"video",
+
 
     async parse(url){
 
-        return {
 
-            title:"",
+        try{
 
-            cover:"",
 
-            video:"",
+            const response = await axios.post(
+                
+                "https://qyapi.ipaybuy.cn/api/video",
 
-            message:"抖音解析模块已连接"
+                {
 
-        };
+                    appId: process.env.QIYUN_APPID,
+
+                    appKey: process.env.QIYUN_APPKEY,
+
+                    url:url
+
+                },
+
+                {
+
+                    headers:{
+
+                        "Content-Type":"application/json"
+
+                    }
+
+                }
+
+            );
+
+
+            const result=response.data;
+
+
+            if(result.code !== 200){
+
+                return {
+
+                    message:result.msg || "解析失败"
+
+                };
+
+            }
+
+
+
+            return {
+
+
+                title:
+                result.data.title || "",
+
+
+                cover:
+                result.data.cover_url || "",
+
+
+                video:
+                result.data.video_url || "",
+
+
+                author:
+                result.data.author || {}
+
+
+            };
+
+
+
+        }catch(error){
+
+
+            return {
+
+                message:"接口请求失败",
+
+                error:error.message
+
+            };
+
+
+        }
+
 
     }
+
 
 };
