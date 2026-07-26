f760f077874a94411a30795cd9df00ede3e9a028
xiangxiang6666 <872388587@qq.com>
2026-07-26 10:54:20 +0800

    Create douyin.js

---
+++ parsers/douyin.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    name:"douyin",
+
+    async parse(url){
+
+        return {
+
+            title:"",
+
+            cover:"",
+
+            video:"",
+
+            message:"抖音解析模块已连接"
+
+        };
+
+    }
+
+};
