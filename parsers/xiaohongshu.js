d8db837786f5ff4d471be46b9989db8d03b19284
xiangxiang6666 <872388587@qq.com>
2026-07-26 10:55:40 +0800

    Create xiaohongshu.js

---
+++ parsers/xiaohongshu.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    name:"xiaohongshu",
+
+    async parse(url){
+
+        return {
+
+            title:"",
+
+            cover:"",
+
+            video:"",
+
+            message:"小红书解析模块已连接"
+
+        };
+
+    }
+
+};
