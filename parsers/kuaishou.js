0146899f23a07e9ae2b1db142ba39bb184fa1f2e
xiangxiang6666 <872388587@qq.com>
2026-07-26 10:54:58 +0800

    Create kuaishou.js

---
+++ parsers/kuaishou.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    name:"kuaishou",
+
+    async parse(url){
+
+        return {
+
+            title:"",
+
+            cover:"",
+
+            video:"",
+
+            message:"快手解析模块已连接"
+
+        };
+
+    }
+
+};
